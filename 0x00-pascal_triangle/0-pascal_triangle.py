114248426afeb175a9bf53d62c7876d74a873811
pouch <poucheuwilfried1@gmail.com>
2024-10-03 18:14:04 -0700

    fix return type

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,17 +4,21 @@ determine a tree of the pascal trinangle
 '''
 
 
-def pascal(n):
+def pascal_triangle(n):
     '''
     n: height of the pascal tree
     '''
+    if n <= 0:
+        return []
+    pas = []
     ancien = [1]
     nouveau = ancien.copy()
     for i in range(n):
-        print(nouveau)
+        pas.append(nouveau)
         ancien = nouveau.copy()
         ancien.append(0)
         nouveau = ancien.copy()
         nouveau[0] = 1
         for j in range(1, len(ancien)):
             nouveau[j] = ancien[j-1] + ancien[j]
+    return pas

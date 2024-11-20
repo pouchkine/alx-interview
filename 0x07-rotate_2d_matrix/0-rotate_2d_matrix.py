f9b6ed05353e096101ff0abd8ad3bdfd83f8069f
pouchkine <pouchkinecamerdouala@gmail.com>
2024-11-20 10:55:06 +0200

    fix inplace matrice matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,4 +9,6 @@ def rotate_2d_matrix(matrix):
     """
     n_col = len(matrix[0])
     matrix.reverse()
-    matrix = [[ligne[i] for ligne in matrix] for i in range(n_col)]
+    a = [[ligne[i] for ligne in matrix] for i in range(n_col)]
+    for i in range(n_col):
+        matrix[i] = a[i]
